c1f14eb50da8bf2db0fa29488cb93799895babb9
dvgorsky <48658126+dvgorsky@users.noreply.github.com>
2023-10-20 13:18:39 +0300

    CW #52 class-reflection-scenarios

---
+++ src/telran/reflection/MethodCallByNameAppl.java
@@ -0,0 +1,25 @@
+package telran.reflection;
+
+import java.lang.reflect.Method;
+
+public class MethodCallByNameAppl {
+
+	public static void main(String[] args) throws Exception {
+		if(args.length < 1) {
+			System.out.println("usage: argument must contain method name");
+		} else {
+			X obj = new X();
+			Class<?> clazz =  obj.getClass();
+//			Method [] methods = clazz.getDeclaredMethods();
+//			for(Method method: methods) {
+//				System.out.println(method.getName());
+//			}
+			Method method = clazz.getDeclaredMethod(args[0], int.class);
+			method.setAccessible(true);
+			String res = (String) method.invoke(obj, 15);
+			System.out.println(res);
+		}
+
+	}
+
+}
